26c366cdbf69807d5bb2c5ba1c081e7029029b55
TheeoCornnaro <TheeoCornnaro@users.noreply.github.com>
2021-10-17 05:13:00 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="TheeoCornnaro"
+original_name="code_exercises"
+original_urlname="code_exercises"
+original_description="Awesome code_exercises created by TheeoCornnaro"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv code_exercises $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-"""Python setup.py for project_name package"""
+"""Python setup.py for code_exercises package"""
 import io
 import os
 from setuptools import find_packages, setup
@@ -6,7 +6,7 @@ from setuptools import find_packages, setup
 
 def read(*paths, **kwargs):
     """Read the contents of a text file safely.
-    >>> read("project_name", "VERSION")
+    >>> read("code_exercises", "VERSION")
     '0.1.0'
     >>> read("README.md")
     ...
@@ -30,17 +30,17 @@ def read_requirements(path):
 
 
 setup(
-    name="project_name",
-    version=read("project_name", "VERSION"),
-    description="project_description",
-    url="https://github.com/author_name/project_urlname/",
+    name="code_exercises",
+    version=read("code_exercises", "VERSION"),
+    description="Awesome code_exercises created by TheeoCornnaro",
+    url="https://github.com/TheeoCornnaro/code_exercises/",
     long_description=read("README.md"),
     long_description_content_type="text/markdown",
-    author="author_name",
+    author="TheeoCornnaro",
     packages=find_packages(exclude=["tests", ".github"]),
     install_requires=read_requirements("requirements.txt"),
     entry_points={
-        "console_scripts": ["project_name = project_name.__main__:main"]
+        "console_scripts": ["code_exercises = code_exercises.__main__:main"]
     },
     extras_require={"test": read_requirements("requirements-test.txt")},
 )
