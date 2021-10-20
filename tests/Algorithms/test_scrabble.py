001922b31b6a27b4ccf9f8561879c428b3c80172
Yeyo <diego.jove.prado@gmail.com>
2021-10-19 23:10:38 -0400

    scrabble + tests - input data + json files

--- tests/Algorithms/test_scrabble.py
+++ tests/Algorithms/test_scrabble.py
@@ -1,4 +1,4 @@
-from src.code_exercises.algorithms import ghost_legs
+from src.code_exercises.algorithms import scrabble
 from pathlib import Path
 import json
 import pytest
@@ -7,19 +7,20 @@ import mock
 
 # Importing Test Data - Structure -> [inputs], output
 base_path = Path(__file__).parent
-with open(base_path / 'json/ghost_legs_data.json', 'r') as f:
+with open(base_path / 'json/scrabble_data.json', 'r') as f:
     test_data = json.load(f)
 
-# Test
+# Test Parametrization
 
 
 @ pytest.mark.parametrize('input, output', test_data)
-def test_ghost_legs(monkeypatch, capfd, input, output):
+def test_scrabble(monkeypatch, capfd, input, output):
     with mock.patch('builtins.input', side_effect=input):
-        ghost_legs()
+        t = scrabble()
     out, err = capfd.readouterr()
     assert out == output
 
 
+# execute test
 if __name__ == '__main__':
     pytest.main()
